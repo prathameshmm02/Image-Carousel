f71c3d74d427a4d5fa7b47696901a59f8a26b6a8
Prathamesh More <prathameshmm02@gmail.com>
2020-10-21 15:40:44 +0530

    LoadingFix
    Fixed Loading Time by compressing some images and added drop shadow.

--- slideshow.js
+++ slideshow.js
@@ -1,12 +1,10 @@
 var slidenos = [0, 1, 2, 3, 4];
 const slides = document.getElementsByTagName("img");
 console.log(slides);
-var scroll = true;
 function plusSlides(n) {
   showSlides((slidenos[2] += n));
 }
 function showSlides(n) {
-  console.log(slides);
   if (slidenos[2] > 4) {
     slidenos[2] = 0;
   }
@@ -39,9 +37,13 @@ function showSlides(n) {
       "last-slide"
     );
   }
+
   slides[slidenos[0]].classList.add("first-slide");
   slides[slidenos[1]].classList.add("prev-slide");
   slides[slidenos[2]].classList.add("slide");
   slides[slidenos[3]].classList.add("next-slide");
   slides[slidenos[4]].classList.add("last-slide");
+
+  slides[slidenos[1]].addEventListener("click", plusSlides);
+  slides[slidenos[3]].addEventListener("click", plusSlides);
 }
